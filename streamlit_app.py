d77e8037c376b20e1819d0741e320e3086942c76
snowflakeshubhamhatekar <125507461+snowflakeshubhamhatekar@users.noreply.github.com>
2023-02-25 01:16:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,7 @@ except URLError as e:
 # fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # # write your own comment - what does this do?
 # streamlit.dataframe(fruityvice_normalized)
-streamlit.stop()
+#streamlit.stop()
 # my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 # my_cur = my_cnx.cursor()
 # my_cur.execute("select * from fruit_load_list")
