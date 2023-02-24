82c765c4402097f942c087071be123409410be3c
snowflakeshubhamhatekar <125507461+snowflakeshubhamhatekar@users.noreply.github.com>
2023-02-25 00:18:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,8 +45,8 @@ try:
 #     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice) 
 #     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #     streamlit.dataframe (fruityvice_normalized)
-except URLError as e:
-  streamlit.error()
+# except URLError as e:
+#   streamlit.error()
 
 # fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 # streamlit.write('The user entered ', fruit_choice)
@@ -61,7 +61,7 @@ except URLError as e:
 # streamlit.dataframe(fruityvice_normalized)
 
 # don't run anything past here while we troubleshoot
-streamlit.stop()
+#streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
